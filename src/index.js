5d998079d103cc7cc64328255825f0f534076e50
redgektor <redgektor11@gmail.com>
2018-02-22 23:58:51 +0300

    18 from 25 done.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    // сортируем входящий массив
+    for (var i = indices.length; i >= 0; i--) {
+        for (var j = 0; j < indices.length; j++) {
+            if ( indices[j] > indices[j+1] ) {
+                var x = indices[j+1];
+                indices[j+1] = indices[j];
+                indices[j] = x;
+            } 
+        }
+    }
+
+    for (var i = 0; i < indices.length; i++) {
+        for (var j = 0; j < indices.length; j++) {
+            if ( this.array[indices[j]] > this.array[indices[j+1]] ) {
+                var x = this.array[indices[j+1]];
+                this.array[indices[j+1]] = this.array[indices[j]];
+                this.array[indices[j]] = x;
+            }
+        }
+    }
   }
 
   setComparator(compareFunction) {
