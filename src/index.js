f0a9f3a15b369396341584f52428444255a47e87
Вадим Мороз <redgektor11@gmail.com>
2018-10-13 19:54:29 +0300

    perf: Conclusion with let/const

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 class Sorter {
   constructor() {
     this.array = [];
+    this.comparator = (a, b) => a - b;
   }
 
   add(element) {
@@ -20,18 +21,17 @@ class Sorter {
   }
 
   sort(indices) {    
-  	indices.sort(function compareNumbers(a, b) {return a - b;});
-    var compareArray = [];
-    for (var i = 0; i < indices.length; i++) {
-        compareArray.push(this.array[indices[i]]);
+  	const sortedIndices = indices.sort((a, b) => a - b);
+    let compareArray = [];
+
+    for (let i = 0, len = sortedIndices.length; i < len; i++) {
+        compareArray.push(this.array[sortedIndices[i]]);
     }
-    if (this.comparator == undefined) {
-        compareArray.sort( function compareNumbers(a, b) {return a - b;})
-    } else {
-        compareArray.sort(this.comparator);
-    }
-    for (var i = 0; i < compareArray.length; i++) {
-        this.array.splice(indices[i], 1, compareArray[i]); 
+
+    compareArray.sort(this.comparator);
+
+    for (let i = 0, len = compareArray.length; i < len; i++) {
+        this.array.splice(sortedIndices[i], 1, compareArray[i]); 
     }
   }
 
